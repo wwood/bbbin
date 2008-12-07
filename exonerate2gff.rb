c2d31362b51ef6b59cad8228b64942e45e88d33a
Ben J Woodcroft <donttrustben near gmail.com>
2008-12-08 10:20:56 +1100

    added exonerate2gff, but it sucks right now

---
+++ exonerate2gff.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+#
+# Takes the output of an exonerate run (that has been run with the 
+# --showtargetgff or the other showgff and gives prints out just the GFF part of
+# the file
+ 
+
+START = 
