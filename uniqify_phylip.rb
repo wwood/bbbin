29d97478356c6b836ff10183d370a042ba269409
Ben J Woodcroft <donttrustben near gmail.com>
2009-03-15 21:32:22 +1100

    first bayesian steps

--- uniqify_phylip.rb
+++ uniqify_phylip.rb
@@ -2,7 +2,28 @@
 
 # make all the sequence names uniq in a PHYLIP file
 
-NAME_CHARS = 10
+DEFAULT_NAME_CHARS = 10
+
+require 'optparse'
+
+# ununiqify_ensembl_tree.rb ../ensembl.cbm48.fa uniqued.phylip consense.outtree
+USAGE = "Usage: uniqify_phylip [-n] <phylip_file>"
+options = {
+  :name_chars => DEFAULT_NAME_CHARS
+}
+OptionParser.new do |opts|
+  opts.banner = USAGE
+
+  opts.on("-n", "--name-chars [NUMBER_CHARACTERS]", Integer, "Number of characters to print out per definition chunk") do |v|
+    options[:name_chars] = v
+  end
+end.parse!
+
+unless ARGV.length == 1
+  $stderr.puts USAGE
+  exit 1
+end
+name_chars = options[:name_chars]
 
 number = 0
 first = true
@@ -24,14 +45,14 @@ ARGF.each do |line|
       name = ''
       begin #until there is really a unique name
         number += 1
-        spaces = Math::log10(number).floor+2
+        spaces = Math::log10(number).floor+2+(10-name_chars)
         
-        (0..(NAME_CHARS-spaces)).each do |i|
+        (0..(name_chars-spaces)).each do |i|
           name<< line[i..i]
         end
         name<< "#{number}"
         name<< ' '
-        name<< line[NAME_CHARS..(line.length-1)]
+        name<< line[10..(line.length-1)]
       end while taken_names[name]
       
       taken_names[name] = true
