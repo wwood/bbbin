f6e462822731e0947c1b4c50ed7d8890ebab06f0
Ben J Woodcroft <donttrustben near gmail.com>
2009-04-20 10:53:44 +1000

    finished the uniq.rb library, tested

--- uniq.rb
+++ uniq.rb
@@ -1,4 +1,18 @@
 # A library to help make strings unique, because many bioinformatic
 # programs cannot handle 
 class Uniq
+  def initialize()
+    @names_taken ||= {}
+  end
+  
+  def make_unique(string)
+    number = 0
+    to_return = string #gets changed iff it is not unique.
+    while @names_taken[to_return]
+      number += 1
+      to_return = "#{string}-#{number}";
+    end
+    @names_taken[to_return] = true
+    return to_return
+  end
 end
