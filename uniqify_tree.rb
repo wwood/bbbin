78ce889d4452cbc04dbf9a391d9c59aadcb72f09
Ben J Woodcroft <donttrustben near gmail.com>
2009-04-20 10:59:35 +1000

    working on uniqify_tree.rb, something is wrong

--- uniqify_tree.rb
+++ uniqify_tree.rb
@@ -1,6 +1,17 @@
-# 
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
- 
+#!/usr/bin/env ruby
 
-puts "Hello World"
+# Make each node in a tree have a unique name - otherwise FigTree at least
+# cannot handle it
+
+require 'bio'
+require '/home/ben/bin/uniq'
+
+tree = Bio::FlatFile.open(ARGF).entries[0].tree
+uniq = Uniq.new
+
+tree.each_node do |node|
+  #I get internal nodes here - not sure how else to skip
+  next if node.name.nil? or node.name.length == 0
+  
+  node.name = uniq.make_unique(node.name);
+end
