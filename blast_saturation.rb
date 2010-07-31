c091f9e49216ac71b12bec5d17ceed2ecf78cb7d
Ben J. Woodcroft <donttrustben@gmail.com>
2010-07-31 22:31:35 +1000

    blast_saturation.rb now supports blast+ but now -f and -b flags are required for running it - ARGV is no longer sufficient

--- blast_saturation.rb
+++ blast_saturation.rb
@@ -1,18 +1,38 @@
 #!/usr/bin/env ruby
 
-# This script is similar to blast. The difference is that it tries to remove 
+# This script is similar to the original NCBI blast. The difference is that it tries to remove 
 # problems with one area of the protein hogging all the hits.
 #
 
+ACCEPTABLE_BLAST_PLUS_PROGRAMS = %w(blastp blastn tblastn tblastx blastx)
 
 class Blaster
+  # Using (if true) BLAST+ or (if false) simply the older blast2 suite of programs?
+  attr_reader :blast_plus_program
+  attr_reader :verbose
+  
+  def initialize(blast_plus_program=nil)
+    @blast_plus_program = blast_plus_program
+    @verbose = true
+  end
+  
+  def hush
+    @verbose = false
+  end
+  
   def blast(fasta_filename, blast_options, output_filename, blast_iteration_number)
-    $stderr.puts "Running blast ##{blast_iteration_number}, starting at #{`date`}"
+    $stderr.puts "Running blast ##{blast_iteration_number}, starting at #{`date`}" if @verbose
     # Do the actual blast
-    cmd = "blastall -m 8 -i '#{fasta_filename}' -o #{output_filename} #{blast_options}"
+    cmd = nil
+    if @blast_plus_program.nil?
+      cmd = "blastall -m 8 -i '#{fasta_filename}' -o #{output_filename} #{blast_options}"
+    else
+      cmd = "#{@blast_plus_program} -outfmt 6 -query '#{fasta_filename}' -out #{output_filename} #{blast_options}"
+    end
+    $stderr.puts "BLAST CMD LINE$ #{cmd}" if @verbose
     `#{cmd}`
     print `cat #{output_filename}` #output to the user each of the blast results
-    $stderr.puts "Blast ##{blast_iteration_number} returned #{`wc -l #{output_filename}`.to_i} result lines"
+    $stderr.puts "Blast ##{blast_iteration_number} returned #{`wc -l #{output_filename}`.to_i} result lines" if @verbose
   end
 end
 
@@ -23,31 +43,54 @@ if __FILE__ == $0
   
   # Parse options
   USAGE = [
-  'Usage: blast_saturation.rb <fasta_filename> <blast_options>',
-  '  where <fasta_filename> is the fasta file of sequences to blast, and <blast_options> is a string you might give to blastall.',
-  '   <blast_options> must include -d and -p, but cannot include -m or -i'
+  'Usage: blast_saturation.rb [-p <blast+_program>] -f <fasta_filename> -b <blast_options>',
+  
   ]
-  #  options = {
-  #  :print_masked_sequences_only => false,
-  #  }
-  #  o = OptionParser.new do |opts|
-  #    opts.banner = USAGE
-  #    
-  #    opts.on("-m", "--masked-only", "Print out only those sequences that have blast hits (or equivalently, only those that are masked") do |v|
-  #      options[:print_masked_sequences_only] = true
-  #    end
-  #  end
-  #  o.parse!
+  options = {
+    :blast_plus_program => nil,
+    :verbose => true
+  }
+  require 'optparse'
+  o = OptionParser.new do |opts|
+    opts.banner = USAGE
+    
+    opts.on('-f','--fasta-filename FASTA_FILENAME', "fasta filename of sequences to BLAST") do |v|
+      options[:fasta_filename] = v
+    end    
+    
+    verbose = "Options to pass to the blast program. For BLAST+ usage (-p), required argument is -db, illegal arguments that give undefined behaviour are -out, -outfmt or -query. If using the older version of blast, -p and -d are required, while -m, -o and -i are illegal."
+    opts.on('-b','--blast-options BLAST_OPTIONS', verbose) do |v|
+      #TODO: make the illegal things really illegal by catching them
+      options[:blast_options] = v
+    end
+    
+    opts.on("-p", "--blast_plus_program [PROGRAM]", "Use the specified blast+ program instead of the previous BLAST2 version.") do |v|
+      unless ACCEPTABLE_BLAST_PLUS_PROGRAMS.include?(v)
+        $stderr.puts "Unexpected blast+ program found: `#{v}'. Expected one of #{ACCEPTABLE_BLAST_PLUS_PROGRAMS.sort.join(", ")}."
+        exit 1
+      end
+      options[:blast_plus_program] = v
+    end
+    
+    opts.on('-q','--quiet','Opposite of verbose') do
+      options[:verbose] = false
+    end
+  end
+  o.parse!
   
   # require fasta input
   # should be able to set arbitrary blast parameters (required, otherwise blast will not work - db and program at least needed).
-  unless ARGV.length == 2
+  unless options[:fasta_filename] and options[:blast_options]
     $stderr.puts USAGE
     exit 1
   end
-  fasta_filename = ARGV[0]
-  blast_options = ARGV[1]
-  blaster = Blaster.new
+  fasta_filename = options[:fasta_filename]
+  blast_options = options[:blast_options]
+  
+  # setup blaster, which is differently done when blast+ has been specified
+  blaster = Blaster.new options[:blast_plus_program]
+  blaster.hush unless options[:verbose]
+  
   # should be able to set number deep at each point #TODO
   
   blast_iteration_number = 1
@@ -61,7 +104,7 @@ if __FILE__ == $0
   # while there are proteins still being hit
   while File.size(blast_result_file.path) > 0
     # mask out the hit areas
-    $stderr.puts "Masking the fasta file parts that have blast hits, starting at #{`date`}"
+    $stderr.puts "Masking the fasta file parts that have blast hits, starting at #{`date`}" if options[:verbose]
     masked_fasta_file = Tempfile.new("masked_fasta#{blast_iteration_number}")
     `blast_mask.rb -m #{last_masked_file.path} #{blast_result_file.path} >#{masked_fasta_file.path}`
     blast_iteration_number += 1
--- blast_saturation_test.rb
+++ blast_saturation_test.rb
@@ -10,6 +10,14 @@ class BlastSaturationTest < Test::Unit::TestCase
     `formatdb -i #{@db.path}`
   end
   
+  # create blast+ databases
+  def create_formatted_blast_plus_database(fasta_string)
+    @db = Tempfile.new('db')
+    @db.puts fasta_string
+    @db.close
+    `makeblastdb -in #{@db.path}`
+  end
+  
   def test_no_hits
     create_formatted_blast_database <<EOF
 >seq1
@@ -19,13 +27,13 @@ AGAGAGAGAGAGAGA
 >jgi|Triad1|62822|fgeneshTA2_pg.C_scaffold_1150000001_modified
 MSDIKKFKKELDTFHNSEATRKSSNKIINVICAKSDNYIGGSADLSGSNGTKCN
 EOF
-
+    
     Tempfile.open('fasta') do |t|
       t.puts '>notHitting'
       t.puts 'GGGGGGGGG'
       t.close
       Tempfile.open('output') do |out|
-        `blast_saturation.rb #{t.path} '-p blastp -d #{@db.path} -F F -z 10000' >#{out.path}`
+        `blast_saturation.rb -q -f #{t.path} -b '-p blastp -d #{@db.path} -F F -z 10000' >#{out.path}`
         output = `cat #{out.path}`
         assert_equal '', output
       end
@@ -48,7 +56,7 @@ EOF
       t.close
       Tempfile.open('output') do |out|
         # use -z 100 so that the db can be changed without minor e-value changes
-        `blast_saturation.rb #{t.path} '-p blastp -d #{@db.path} -F F -z 100' >#{out.path}`
+        `blast_saturation.rb -q -f #{t.path} -b '-p blastp -d #{@db.path} -F F -z 100' >#{out.path}`
         output = File.open(out.path).read
         assert_equal [
         'jgi|Triad1|62822|fgeneshTA2_pg.C_scaffold_1150000001',
@@ -78,13 +86,13 @@ KSFGGTFLVFSD
 >second
 FFLVSLSLASIPAVNMAMYLEKKVSYLYEASDGF
 EOF
-    
+
     Tempfile.open('fasta') do |t|
       t.puts '>jgi|Triad1|62822|fgeneshTA2_pg.C_scaffold_1150000001'
       t.puts 'MSDIKKFKKELDTFHNSEATRKSSNKIINVICAKSDNYIGGSADLSGSNGTKCNNHKIISKEDFTGNYIHYGVREHAMVAIMNGINIHDKFKSFGGTFLVFSDFFLVSLSLASIPAVNMAMYLEKKVSYLYEASDGF'
       t.close
       
-      output = `blast_saturation.rb #{t.path} '-p blastp -d #{@db.path} -F F -z 10000 -b 1 -v 1'`
+      output = `blast_saturation.rb -q -f #{t.path} -b '-p blastp -d #{@db.path} -F F -z 10000 -b 1 -v 1'`
       expected = <<EOF
 jgi|Triad1|62822|fgeneshTA2_pg.C_scaffold_1150000001	jgi|Triad1|62822|fgeneshTA2_pg.C_scaffold_1150000001_modified	100.00	54	0	0	1	54	1	54	6e-28	 110
 jgi|Triad1|62822|fgeneshTA2_pg.C_scaffold_1150000001	second	100.00	34	0	0	104	137	1	34	2e-15	68.9
@@ -92,5 +100,44 @@ jgi|Triad1|62822|fgeneshTA2_pg.C_scaffold_1150000001	third	100.00	12	0	0	92	103	
 EOF
       assert_equal expected, output
     end
+  end
+  
+  def test_blast_plus_bad_program
+    Tempfile.open('err') do |t|
+      `blast_saturation.rb -p notaBLASTprogram '-d nowhere' 2>#{t.path}`
+      expected = <<EOF
+Unexpected blast+ program found: `notaBLASTprogram'. Expected one of blastn, blastp, blastx, tblastn, tblastx.
+EOF
+      assert_equal expected, t.read
+    end
+  end
+  
+  def test_blast_plus_three_hits
+    create_formatted_blast_plus_database <<EOF
+>seq1
+ATGCATGCATGC
+>seq2
+AGAGAGAGAGAGAGA
+>jgi|Triad1|62822|fgeneshTA2_pg.C_scaffold_1150000001_modified
+MSDIKKFKKELDTFHNSEATRKSSNKIINVICAKSDNYIGGSADLSGSNGTKCN
+>third
+KSFGGTFLVFSD
+>second
+FFLVSLSLASIPAVNMAMYLEKKVSYLYEASDGF
+EOF
+    
+    Tempfile.open('fasta') do |t|
+      t.puts '>jgi|Triad1|62822|fgeneshTA2_pg.C_scaffold_1150000001'
+      t.puts 'MSDIKKFKKELDTFHNSEATRKSSNKIINVICAKSDNYIGGSADLSGSNGTKCNNHKIISKEDFTGNYIHYGVREHAMVAIMNGINIHDKFKSFGGTFLVFSDFFLVSLSLASIPAVNMAMYLEKKVSYLYEASDGF'
+      t.close
+      
+      output = `blast_saturation.rb -q -p blastp -f #{t.path} -b '-db #{@db.path} -seg no -dbsize 10000 -num_descriptions 1 -num_alignments 1'`
+      expected = <<EOF
+jgi|Triad1|62822|fgeneshTA2_pg.C_scaffold_1150000001	jgi|Triad1|62822|fgeneshTA2_pg.C_scaffold_1150000001_modified	100.00	54	0	0	1	54	1	54	6e-28	 110
+jgi|Triad1|62822|fgeneshTA2_pg.C_scaffold_1150000001	second	100.00	34	0	0	104	137	1	34	2e-15	68.9
+jgi|Triad1|62822|fgeneshTA2_pg.C_scaffold_1150000001	third	100.00	12	0	0	92	103	1	12	0.007	26.9
+EOF
+      assert_equal expected, output
+    end    
   end
 end
