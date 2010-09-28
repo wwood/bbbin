32f461311e0c1ba70195a661f47d00a5fc236119
Ben J Woodcroft <donttrustben near gmail.com>
2010-09-28 14:12:32 +1000

    mask_eupathdb_domains.rb now tested

--- mask_eupathdb_domains.rb
+++ mask_eupathdb_domains.rb
@@ -2,7 +2,7 @@
 
 class EuPathDB
   class Domain
-    attr_accessor :start, :stop, :protein_accession, :domain_accession, :source, :evalue
+    attr_accessor :start, :stop, :protein_accession, :domain_accession, :domain_name, :source, :evalue
   end
   
   class DomainAnnotationFile
@@ -14,10 +14,12 @@ class EuPathDB
     # accession numbers to arrays of domain objects
     def domain_hash
       hash = {}
-      io.each_line do |line|
+      @io.each_line do |line|
         domain = parse_domain_from_annotation_line(line)
-        hash[domain.protein_accession] ||= []
-        hash[domain.protein_accession].push domain
+        unless domain.nil?
+          hash[domain.protein_accession] ||= []
+          hash[domain.protein_accession].push domain
+        end
       end
       hash
     end
@@ -26,13 +28,16 @@ class EuPathDB
     # (tab separated, but there is extra whitespace on the e-value)
     # PFI1830c  PFAM  PF03011 PFEMP 642 810   1.4E-49
     def parse_domain_from_annotation_line(annotation_line)
+      splits = annotation_line.split("\t")
       d = Domain.new
-      d.protein_accession = annotation_line[0].strip
-      d.source = annotation_line[1].strip
-      d.domain_accession = annotation_line[2].strip
-      d.start = annotation_line[3].strip.to_i
-      d.stop = annotation_line[4].strip.to_i
-      d.evalue = annotation_line[5].strip
+      d.protein_accession = splits[0].strip
+      d.source = splits[1].strip
+      d.domain_accession = splits[2].strip
+      d.domain_name = splits[3].strip
+      d.start = splits[4].strip.to_i
+      d.stop = splits[5].strip.to_i
+      d.evalue = splits[6].strip.to_f
+      d
     end
   end
 end
--- mask_eupathdb_domains_test.rb
+++ mask_eupathdb_domains_test.rb
@@ -5,12 +5,28 @@ class EuPathDBDomainAnnotationFileTest < Test::Unit::TestCase
   
   def test_parse_line
     line = <<EOF
-PFI1830c  PFAM  PF05658 Hep_Hag 1894  1921    3.3E-05
+PFI1830c	SUPERFAMILY	SSF101967	0042205	1895	2072	  1.7E-03
 EOF
-    obs = EuPathDB::DomainAnnotationFile.new
-    assert_kind_of EuPathDB::Domain, obs
-    assert_equal 'PFI1830c', obs.protein_accession
-    assert_equal 1894, obssstart
-    assert_equal 1921, obs.stop
+    obs = EuPathDB::DomainAnnotationFile.new(line)
+    domains = obs.domain_hash
+    assert_kind_of Hash, domains
+    assert domains['PFI1830c']
+    assert_kind_of Array, domains['PFI1830c']
+    assert_equal 1, domains['PFI1830c'].length
+    d = domains['PFI1830c'][0]
+    assert_equal 'PFI1830c', d.protein_accession
+    assert_equal 1895, d.start
+    assert_equal 2072, d.stop
+    assert_equal 1.7e-3, d.evalue
+  end
+  
+  def test_parse_file
+    obs = EuPathDB::DomainAnnotationFile.new(File.open(File.join('data','plasmodb_domain_definition_extract.txt')))
+    domains = obs.domain_hash
+    assert_equal 2, domains.keys.length
+    assert_equal 2, domains['PFI1830c'].length
+    assert_equal 1, domains['PFI1825w'].length
+    assert_equal 1.7E-03, domains['PFI1830c'][1].evalue
+    assert_equal 0.0, domains['PFI1825w'][0].evalue
   end
 end
