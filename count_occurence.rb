fbc110c2efccc795b2564a5f1f68ffaa7898c776
Ben J. Woodcroft <donttrustben@gmail.com>
2010-09-29 20:28:11 +1000

    small changes to orf_finder and count_occurence.rb now works better for single sequences

--- count_occurence.rb
+++ count_occurence.rb
@@ -3,44 +3,62 @@
 # Count the total occurance of each letter (i.e. amino acid or nucleotide) in the
 # input fasta file.
 
- require 'rubygems'
- require 'bio'
- require 'pp'
- require 'optparse'
+require 'rubygems'
+require 'bio'
+require 'pp'
+require 'optparse'
 
- hash = {}
+hash = {}
 # d - use dipeptides
 # i - report occurences for each sequence individually
-options = ARGV.getopts("d",'i:')
- 
- Bio::FlatFile.foreach(ARGF) do |entry|
-last = nil
-seq = entry.seq
-   seq.each_char do |char|
-if options['d'] # d for dipeptides
-if last
-i = "#{last}#{char}"
-hash[i] ||= 0
-hash[i] += 1
-end
-last = char
-else
-     hash[char] ||= 0
-     hash[char] += 1
+options = ARGV.getopts("d",'i')
+
+if options['i'] and options['d']
+  $stderr.puts "Cannot use -i and -d options together at the moment."
+  exit 1
 end
 
-
-   end
-# if reporting for each seq, report now
+amino_acids = Bio::AminoAcid::Data::WEIGHT.keys
+# Print headers for 1 sequence at a time
 if options['i']
-result = hash[options['i']]
-if result.nil?
-puts 0
-else
-puts result
+  puts [
+  'Identifier',
+  amino_acids
+  ].flatten.join("\t")
 end
-hash = {}
+
+Bio::FlatFile.foreach(ARGF) do |entry|
+  last = nil
+  seq = entry.seq
+  seq.each_char do |char|
+    if options['d'] # d for dipeptides
+      if last
+        i = "#{last}#{char}"
+        hash[i] ||= 0
+        hash[i] += 1
+      end
+      last = char
+    else
+      hash[char] ||= 0
+      hash[char] += 1
+    end
+  end
+  
+  # if reporting for each seq, report now
+  if options['i']
+    if hash.empty?
+      $stderr.puts "Ignoring #{entry.definition} since it appears to have zero length"
+    else
+      print entry.definition
+      amino_acids.each do |aa|
+        to_print = hash[aa]
+        to_print = 0 if to_print.to_s=='' #print 0 not nothing to make later analyses easier 
+        print "\t#{to_print}"
+      end
+      puts
+    end
+    hash = {} # reset the hash for the next sequence
+  end
 end
- end
- 
- pp hash unless options['i']
+
+pp hash unless options['i']
