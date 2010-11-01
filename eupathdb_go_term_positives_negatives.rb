f7fb8e9efb092a2a2c00e8a9b02bff605dfd3995
Ben J. Woodcroft <donttrustben@gmail.com>
2010-11-01 20:01:46 +1100

    working eupathdb_go_term_positives_negatives.rb

--- eupathdb_go_term_positives_negatives.rb
+++ eupathdb_go_term_positives_negatives.rb
@@ -1,22 +1,52 @@
 #!/usr/bin/env ruby
 
-require 'go'
-require '../forays/eupathdb/eu_path_d_b_gene_information_table'
+require 'rubygems'
+#require 'go' #provided by goruby
+require 'eupathdb_gene_information_table' #provided by the reubypathdb gem
+require '/home/ben/forays/goruby/lib/go'
 
 if __FILE__ == $0
-  # nucleus subsumer
-  subsumer = Bio::Go.new.subsume_tester('GO:0005634')
-  # nucleus parents - these annotations are ignored
-  exclusion_list = %w(GO:0005575
-GO:0005622
-GO:0005623
-GO:0005634
-GO:0043226
-GO:0043227
-GO:0043229
-GO:0043231
-GO:0044424
-GO:0044464)
+  inspected_go = 'GO:0005634'
+  gor = Bio::Go.new
   
-  gene_info_file = EuPathDBGeneInformationTableTest
+  # nucleus subsumer
+  nucleus_subsumer = gor.subsume_tester(inspected_go)
+  cc_subsumer = gor.subsume_tester('GO:0005575')
+  # nucleus' ancestors or ancestors of any of its children. 
+  # These annotations are ignored, because they aren't
+  # positive or negative, merely 'cordial'
+  exclusion_list = gor.cordial_cc(inspected_go)
+  
+  EuPathDBGeneInformationTable.new(ARGF).each do |g|
+    go_terms = g.get_table('GO Terms').collect{|r| r['GO ID']}
+    
+    positive = false
+    negative = false
+    go_terms.each do |d|
+      begin
+        i = gor.primary_go_id d
+        nuc = nucleus_subsumer.subsume?(i)
+        cc = cc_subsumer.subsume?(i)
+        
+        positive = true if nuc 
+        negative = true if cc and !(exclusion_list.include?(i)) and i!=inspected_go
+      rescue RException
+        # ignore when there are strange GO annotations, that don't seem to exist in the database
+      end
+    end
+    
+    # Print the result for this gene
+    print "#{g['Gene Id']}\t"
+    if positive
+      if negative
+        puts 'both'
+      else
+        puts 'positive'
+      end
+    elsif negative
+      puts 'negative'
+    else
+      puts 'no data'
+    end
+  end
 end
