f88fb0291b82edc2619e09f1a7da40ce28e33e90
Ben J Woodcroft <donttrustben near gmail.com>
2011-03-23 17:51:12 +1100

    modularising genbank_newer_accession.rb

--- genbank_newer_accession.rb
+++ genbank_newer_accession.rb
@@ -11,13 +11,39 @@
 require 'rubygems'
 require 'bio'
 
-if __FILE__ == $0
-  Bio::FlatFile.foreach(Bio::GenBank, ARGF) do |me|
-    new_accession = nil
-    
+ENTREZ_DATABASE = 'protein'
+Bio::NCBI.default_email = "go@away.com"
+
+# monkey in the comment to new gi method
+class Bio::GenBank
+  # Return the newer GI for this entry, or nil if none exist. This is encoded in the comment section, using "this sequence was replaced by gi:\d+" where the \d+ is returned. 
+  def updated_gi
+    # remove the breaking of the comment over several lines
     comment_spaceless = me.comment.gsub(/\n\s*/,' ')
+    
     if matches = comment_spaceless.match(/this sequence was replaced by (\S*)\./)
       new_accession = matches[1]
+      
+      # go through it recursively. Is that newer identifier still out of date?
+      if matches2 = matches[1].gsub(/gi:/,'')
+        return newer_gi
+      end
+      
+      # parsing error of comment
+      raise ParseException, "Could not parse comment `#{matches[1]}' from the comment section of #{accession}: #{comment}"
+    end
+  end
+end
+
+
+
+
+if __FILE__ == $0
+  Bio::FlatFile.foreach(Bio::GenBank, ARGF) do |me|
+    new_accession = me.updated_gi
+    
+    if new_accession
+      newer_returned = Bio::NCBI::REST.efetch(newer_gi, {:rettype => 'gb', :db => ENTREZ_DATABASE})
     end
     
     puts [
