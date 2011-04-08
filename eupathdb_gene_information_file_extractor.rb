22812156bd8c18f2061a41ba59111c147637cabf
Ben J Woodcroft <donttrustben near gmail.com>
2011-04-08 13:07:10 +1000

    added script for string-matching enzyme name lists

--- eupathdb_gene_information_file_extractor.rb
+++ eupathdb_gene_information_file_extractor.rb
@@ -7,43 +7,31 @@ require 'optparse'
 require 'rubygems'
 require 'eupathdb_gene_information_table' #from the reubypathdb rubygem
 
-# A class for extracting gene info from a gene info file
-class EuPathDBGeneInformationFileExtractor
-  def initialize(filename = nil)
-    @filename = filename
-  end
-
-  # Returns a EuPathDBGeneInformation object corresponding to the wanted key. If
-  # there are multiple in the file, only the first is returned.
-  def extract_gene_info(wanted_gene_id)
-    EuPathDBGeneInformationTable.new(File.open(@filename)).each do |gene|
-      return gene if wanted_gene_id == gene.get_info('Gene Id')
-    end
-    return nil
-  end
-end
-
 if $0 == __FILE__
   options = {
     :quit_after_first => true,
-    :gene_information_table_path => '/home/ben/phd/data/Toxoplasma gondii/genome/ToxoDB/6.0/TgondiiME49Gene_ToxoDB-6.0.txt'
+    :gene_information_table_path => '/home/ben/phd/data/Toxoplasma gondii/genome/ToxoDB/6.0/TgondiiME49Gene_ToxoDB-6.0.txt',
+    :grep_hack => false,
   }
   o = OptionParser.new do |opts|
     opts.banner = [
       'Usage: eupathdb_gene_information_file_extractor.rb <eupathdb_gene_identifier> <key>'
     ]
+    opts.on("-g", "--grep-hack", "Invoke the grep hack to make gene information file extraction faster") do
+      options[:grep_hack] = true
+    end
     if ARGV.length != 2
       $stderr.puts opts.banner
       exit
     end
   end
   o.parse!
-
+  
   wanted_gene_id = ARGV[0]
   info = ARGV[1]
-
-  gene = EuPathDBGeneInformationFileExtractor.new(options[:gene_information_table_path]).extract_gene_info(wanted_gene_id)
-
+  
+  gene = EuPathDBGeneInformationFileExtractor.new(options[:gene_information_table_path]).extract_gene_info(wanted_gene_id, 1000)
+  
   if gene.get_info(info).nil?
     table = gene.get_table(info)
     if table.nil?
