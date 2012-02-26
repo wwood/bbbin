544956bd77036f447996fde28d52071f0a2f86fd
Ben J. Woodcroft <donttrustben near gmail.com>
2012-02-26 22:11:59 +1000

    messed up join_unsorted I think

--- join_unsorted.rb
+++ join_unsorted.rb
@@ -21,20 +21,29 @@ end
 
 # Go through file 2
 file1_foundeds = {}
+num_splits_in_file1 = nil
 File.foreach(ARGV[1]) do |line|
   splits = line.chomp.split(separator)
+  if num_splits_in_file1.nil?
+    num_splits_in_file1 = splits.length #assume all lines have the same number of splits, fail otherwise
+  elsif num_splits_in_file1 != splits.length
+    raise Exception, "Unexpected number of splits over '#{separator}' found in the first file. I give up so I don't cause you pain later"
+  end
   key = splits[join_field].strip
+  file2_data = splits[1..splits.length-1]
   if file1_hash[key]
     if file1_foundeds[key].nil?
-      puts [file1_hash[key],splits[1..splits.length-1]].flatten.join(separator)
+      puts [file1_hash[key],file2_data].flatten.join(separator)
       file1_foundeds[key] ||= 0
       file1_foundeds[key] += 1
     else
       $stderr.puts "Found key `#{key}' more than once in the second file. Fail."
     end
   else
-    $stderr.puts "Unable to find key `#{key}' in File 1 - I need file 1 to define all the keys in file 2, ignoring"
-    next
+    # Print empty cells for the first file
+    (num_splits_in_file1).times {print separator}
+    # Print the data for the second file
+    puts file2_data.join(separator)
   end
 end
 
--- sam_to_percent_id.rb
+++ sam_to_percent_id.rb
@@ -5,5 +5,14 @@
 
 require 'bio-samtools'
 
-sam = Bio::DB::SAM.open(:tam => ARGF)
+# a SAM line hitting
+# F6F53:106:426	16	gi|374477287|gb|JH600414.1|	6371	0	10M	*	0	0	TTTGCCGTAC	*	XT:A:R	NM:i:0	X0:i:5	X1:i:127	XM:i:0	XO:i:0	XG:i:0	MD:Z:10
+# a SAM line failing
+# F6F53:9:85	4	*	0	0	*	*	0	0	ATCTGGGGACACGGTTCGA	*
+
+
+ARGF.each_line do |line|
+  next if line.match(/^@/) #skip header lines
+  splits = line.strip.split("\t")
+  if 
 
