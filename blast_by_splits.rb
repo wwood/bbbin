c544e4741e9b9f2444092ed863602e265ccb5a74
Ben J. Woodcroft <donttrustben near gmail.com>
2012-03-27 11:42:20 +1000

    blast_by_splits.rb: (thanks @MadsAlbertsen) fix option to output to file so it actually does that, add option to change the number of target sequences that are reported

--- blast_by_splits.rb
+++ blast_by_splits.rb
@@ -10,6 +10,7 @@ require 'optparse'
 
 options = {
   :threads => 24,
+  :max_target_seqs => 1,
 }
 OptionParser.new do |opts|
   opts.banner = "Usage: blast_by_splits.rb --query <query_fasta> --db <blast_database_path>"
@@ -21,13 +22,17 @@ OptionParser.new do |opts|
   opts.on('-d', "--db DB", "path to blast database") do |v|
     options[:db] = v
   end
+  
+  opts.on('-o', "--out OUTPUT_FILE", "File to dump blast results to. Note that the sequences with blast hits are probably not in the same order as the input file") do |v|
+    options[:output_file] = v
+  end
 
   opts.on('-a', "--threads NUM_THREADS", "Number of CPUs to spread the load across") do |v|
     options[:threads] = v.to_i
   end
-  
-  opts.on('-o', "--output OUTPUT_FILE", "File to dump blast results to. Note that the sequences with blast hits are probably not in the same order as the input file") do |v|
-    options[:output_file] = v.to_i
+    
+  opts.on('-t', "--max_target_seqs NUM_SEQS", "How many hits to report for each query sequence? [default: #{options[:max_target_seqs]}") do |v|
+    options[:max_target_seqs] = v.to_i
   end
 end.parse!
 
@@ -35,6 +40,7 @@ raise unless options[:query]
 raise unless options[:db]
 raise unless options[:threads] > 0
 raise Exception, "You must specify an output file (-o/--output)" unless options[:output_file]
+raise Exception, "Unexpected unqualified areguments: #{ARGV.join(' ')}" unless ARGV.length == 0
 
 input_temps = (1..options[:threads]).collect{Tempfile.new('blast_by_splitsIn')}
 
@@ -54,7 +60,7 @@ output_temps = (1..options[:threads]).collect{Tempfile.new('blast_by_splitsOut')
 # Start each of the threads
 blast_threads = (1..options[:threads]).collect do |i|
   Thread.new do
-    `blastp -query '#{input_temps[i-1].path}' -db '#{options[:db]}' -num_descriptions 1 -num_alignments 1 -outfmt 6 -out #{output_temps[i-1].path}`
+    `blastp -query '#{input_temps[i-1].path}' -db '#{options[:db]}' -max_target_seqs #{options[:max_target_seqs]} -outfmt 6 -out #{output_temps[i-1].path}`
   end
 end
 
@@ -69,4 +75,6 @@ blast_threads.each_with_index do |thread, i|
     out.print line
   end
 end
+
+out.close
 
