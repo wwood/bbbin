a9e38ae210f45831cf19fb85eb948e623816017c
Ben J. Woodcroft <donttrustben near gmail.com>
2012-08-01 16:06:59 +1000

    added (tested) script to reversibly unscaffold (ie split on N) a fasta file

---
+++ test/test_unscaffold.rb
@@ -0,0 +1,44 @@
+require 'rspec'
+require 'pp'
+require 'open3'
+
+# To run this test:
+# $ rspec /path/to/test_script_being_tested.rb
+
+# Assumes that the name of the file being tested is ../something.rb, and the name of this script is test_something.rb
+$:.unshift File.join(File.dirname(__FILE__),'..')
+script_under_test = File.basename(__FILE__).gsub(/^test_/,'')
+require script_under_test
+def assert_equal(e,o); o.should eq(e); end
+path_to_script = File.join('..',script_under_test)
+
+
+describe script_under_test do
+  it 'should obvious test' do
+    seqs = %w(>scaffold1 AANNTGTG)
+    
+    Open3.popen3(script_under_test) do |stdin, stdout, stderr|
+      stdin.puts seqs.join("\n")
+      stdin.close
+      
+      err = stderr.readlines
+      raise err unless err == []
+      answer = %w(>scaffold1_1_2 AA >scaffold1_5_8 TGTG).join("\n")+"\n"
+      stdout.read.should eq(answer)
+    end
+  end
+  
+  it 'should handle multiple sequences' do
+    seqs = %w(>scaffold1 AANNTGTG >s2 AAAAAAAA)
+    
+    Open3.popen3(script_under_test) do |stdin, stdout, stderr|
+      stdin.puts seqs.join("\n")
+      stdin.close
+      
+      err = stderr.readlines
+      raise err unless err == []
+      answer = %w(>scaffold1_1_2 AA >scaffold1_5_8 TGTG >s2_1_8 AAAAAAAA).join("\n")+"\n"
+      stdout.read.should eq(answer)
+    end
+  end
+end
---
+++ unscaffold.rb
@@ -0,0 +1,105 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'bio-logger'
+require 'bio'
+
+if __FILE__ == $0 #needs to be removed if this script is distributed as part of a rubygem
+  SCRIPT_NAME = File.basename(__FILE__); LOG_NAME = SCRIPT_NAME.gsub('.rb','')
+  
+  # Parse command line options into the options hash
+  options = {
+    :logger => 'stderr',
+  }
+  o = OptionParser.new do |opts|
+    opts.banner = "
+      Usage: #{SCRIPT_NAME} fasta_file
+      
+      Take a fasta file which has Ns in it (like after a scaffolded assembly), and split
+      wherever there is Ns. Print out a fasta file of the splits, encoding the positions of the Ns into the name
+      
+      e.g. if the input is
+      >scaffold1
+      AANNTGT
+      
+      Then the output would be
+      >scaffold1_1_2
+      AA
+      >scaffold1_5_8
+      TGT
+        \n\n"
+    
+    # logger options
+    opts.on("-q", "--quiet", "Run quietly, set logging to ERROR level [default INFO]") {Bio::Log::CLI.trace('error')}
+    opts.on("--logger filename",String,"Log to file [default #{options[:logger]}]") { |name| options[:logger] = name}
+    opts.on("--trace options",String,"Set log level [default INFO]. e.g. '--trace debug' to set logging level to DEBUG"){|s| Bio::Log::CLI.trace(s)}
+  end
+  o.parse!
+  if ARGV.length > 1
+    $stderr.puts o
+    exit 1
+  end
+  # Setup logging. bio-logger defaults to STDERR not STDOUT, I disagree
+  Bio::Log::CLI.logger(options[:logger]); log = Bio::Log::LoggerPlus.new(LOG_NAME); Bio::Log::CLI.configure(LOG_NAME)
+  
+  
+  Bio::FlatFile.foreach(ARGF) do |scaffold|
+    raise if scaffold.seq[0] == 'N' #not implemented
+    
+    not_N_starts = [0]
+    not_N_stops = []
+    state = 'not_N'
+    
+    # find positions of Ns
+    # working in zero-based indices here
+    position = 0
+    scaffold.seq.to_s.each_char do |char|
+      if state=='not_N' and char == 'N' #switch from non-N mode to N mode
+        state = 'yes_N'
+        not_N_stops.push position-1
+      elsif state == 'yes_N' and char != 'N' #switch from N mode to non-N mode
+        state = 'not_N'
+        not_N_starts.push position
+      end
+      position += 1
+    end
+    not_N_stops.push position-1 #finish it off
+    
+    raise "error check fail. I don't expect to end on an N, for instance" if not_N_starts.length != not_N_stops.length or state != 'not_N'
+    
+    # print out the fragments
+    scaffold_basename = scaffold.entry_id
+    last_stop = 1
+    not_N_starts.each_with_index do |start, index|
+      stop = not_N_stops[index]
+      frag = scaffold.seq[start..stop]
+      puts ">#{scaffold_basename}_#{start+1}_#{stop+1}"
+      puts frag
+    end
+  end
+end #end if running as a script
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
