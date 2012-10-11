e580333e241c7ec09c33ef381d73d9db5f30e837
Ben J Woodcroft <ben@reefedge.(none)>
2012-10-11 21:25:22 +1000

    gly_sliding_window.rb

--- gly_sliding_window.rb
+++ gly_sliding_window.rb
@@ -59,16 +59,25 @@ if __FILE__ == $0
   
   signalp_cleavages = {}
   if options[:signalp_file]
-    # Name NN Prediction HMM Prediction  Predicted?  Cleavege site (if predicted)
-    CSV.foreach(options[:signalp_file],:col_sep => "\t") do |row|
+    headering = true
+    File.open(options[:signalp_file]).each_line do |line|
+      if headering
+        headering = false
+        next
+      end
+      next if line.strip.length == 0
+      # Name NN Prediction HMM Prediction  Predicted?  Cleavege site (if predicted)
+      row = line.strip.split("\t")
       name = row[0]
       cleavage = row[4].to_i
+
       # Check for duplicates
       if signalp_cleavages[name]
         raise Exception, "Unexpectedly found sequence with name #{name.inspect} (at least) twice in the signalp_overview file, giving up"
       end
       signalp_cleavages[name] = cleavage
     end
+    $stderr.puts "Recorded signalp predictions for #{signalp_cleavages.length} sequences, e.g. #{signalp_cleavages.to_a[0].inspect}"
   end
 
   headers = %w(name window_size max_number max_sequence)
@@ -80,14 +89,15 @@ if __FILE__ == $0
     
     # By default use the whole sequence
     sequence = seq.seq
+    name = seq.entry_id
     
     # If a signal peptide was predicted, cleave
-    result = Bio::SignalP::Result.new
+    result = Bio::SignalP::Version3::Result.new
     if options[:signalp_file]
       # bit of a hack - artificially set the signalp value to true
-      signalp_result = signalp_cleavages[seq.entry_id]
+      signalp_result = signalp_cleavages[name]
       if signalp_result.nil?
-        $stderr.puts "Didn't find sequence #{seq.entry_id} in the signalp overview file, is that right?"
+        $stderr.puts "Didn't find sequence #{name} in the signalp overview file, is that right?"
         signalp_result = 0
       end
       result.nn_D_prediction = (signalp_result != 0)
@@ -99,7 +109,7 @@ if __FILE__ == $0
     print [seq.definition, options[:window_size], desc.maximum_counts[:gly], desc.maximum_sequences[:gly]].join("\t")
     if options[:signalp_file]
       print "\t"
-      print signalp_cleavages[seq.entry_id] != 0
+      print signalp_cleavages[name] != 0
     end
     puts
   end
