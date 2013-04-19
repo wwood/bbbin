6169b12553bf4300a67ec9198cdbbde0ba48bd5c
Ben J Woodcroft <donttrustben@gmail.com>
2013-04-19 17:26:03 +1000

    sequence_chunks now can print the ends only. Added (still) failing test, showing usual operation is a bit rubbish

--- sequence_chunks.rb
+++ sequence_chunks.rb
@@ -6,7 +6,7 @@ require 'bio'
 
 if __FILE__ == $0 #needs to be removed if this script is distributed as part of a rubygem
   SCRIPT_NAME = File.basename(__FILE__); LOG_NAME = SCRIPT_NAME.gsub('.rb','')
-  
+
   # Parse command line options into the options hash
   options = {
     :logger => 'stderr',
@@ -16,9 +16,9 @@ if __FILE__ == $0 #needs to be removed if this script is distributed as part of 
   o = OptionParser.new do |opts|
     opts.banner = "
       Usage: #{SCRIPT_NAME} <fasta_file>
-      
+
       Takes an input fasta file, and separates each sequence it into overlapping pieces. Prints the chunks on stdout.\n\n"
-      
+
     opts.on("-c", "--chunk-size ARG", "Size of the chunks to output [default: #{options[:chunk_size]}]") do |arg|
       options[:chunk_size] = arg.to_i
       raise unless options[:chunk_size] > 0
@@ -27,7 +27,10 @@ if __FILE__ == $0 #needs to be removed if this script is distributed as part of 
       options[:step_size] = arg.to_i
       raise unless options[:step_size] > 0
     end
-    
+    opts.on("--ends-only", "Only print the chunks and the start and ends of contigs [default: #{options[:print_ends_only]}]") do
+      options[:print_ends_only] = true
+    end
+
     # logger options
     opts.on("-q", "--quiet", "Run quietly, set logging to ERROR level [default INFO]") {Bio::Log::CLI.trace('error')}
     opts.on("--logger filename",String,"Log to file [default #{options[:logger]}]") { |name| options[:logger] = name}
@@ -40,37 +43,60 @@ if __FILE__ == $0 #needs to be removed if this script is distributed as part of 
   end
   # Setup logging. bio-logger defaults to STDERR not STDOUT, I disagree
   Bio::Log::CLI.logger(options[:logger]); log = Bio::Log::LoggerPlus.new(LOG_NAME); Bio::Log::CLI.configure(LOG_NAME)
-  
+
+  if !options[:print_ends_only] and
+    (options[:step_size]*2 <= options[:chunk_size] or options[:chunk_size] < options[:step_size])
+    raise "Currently buggy and/or not implemented/tested for your thing, sorry."
+  end
+
   window_size = options[:chunk_size]
   step_size = options[:step_size]
-    
+
   print_seq = lambda do |fasta, subseq, start, number_in_sequence, total_fragment_count|
     first_name = "#{fasta.definition.split(/\s/)[0]}_#{number_in_sequence}of#{total_fragment_count}_#{start+1}_#{start+window_size}"
     print '>'
     puts first_name
     puts subseq
   end
-  
+
   Bio::FlatFile.foreach(ARGF) do |seq|
-    fragment_number = 1
-    total_fragment_count = seq.length.to_f/step_size
+    if options[:print_ends_only]
+      if seq.length <= window_size
+        log.warn "Found a sequence #{seq.definition.split(/\s/)[0]} shorter than the window size, hope that's ok with you"
+      end
+      size = window_size
+      size = seq.length if window_size > seq.length
 
-    is_exact_multiple_of_length = ((seq.length.to_f/step_size).round == seq.length/step_size)
-    total_fragment_count += 1 unless is_exact_multiple_of_length #add one for leftovers unless it is exactly on the money 
+      # Print the start
+      first_name = "#{seq.definition.split(/\s/)[0]}_start"
+      print '>'
+      puts first_name
+      puts seq.seq[0...size]
 
-    total_fragment_count = total_fragment_count.to_i
-    
-    last_step = 0
-    0.step(seq.length - window_size, step_size) do |i| 
-      subseq = seq.seq[i, window_size]
-      print_seq.call(seq, subseq, i, fragment_number, total_fragment_count)
-      last_step = i
-      fragment_number += 1
-    end
-    start = seq.length-window_size
-    unless is_exact_multiple_of_length
-      leftover = seq.seq[start .. -1]
-      print_seq.call(seq, leftover, start, fragment_number, total_fragment_count)
+      # Print the end
+      first_name = "#{seq.definition.split(/\s/)[0]}_end"
+      print '>'
+      puts first_name
+      puts seq.seq[seq.length-size...seq.length]
+    else
+      fragment_number = 1
+      total_fragment_count = seq.length/step_size
+
+      is_exact_multiple_of_length = ((seq.length-window_size).modulo(step_size) == 0)
+      total_fragment_count += 1 unless is_exact_multiple_of_length #add one for leftovers unless it is exactly on the money
+
+      last_step = 0
+      0.step(seq.length - window_size, step_size) do |i|
+        subseq = seq.seq[i, window_size]
+        print_seq.call(seq, subseq, i, fragment_number, total_fragment_count)
+        last_step = i
+        fragment_number += 1
+      end
+      start = seq.length-window_size
+      unless is_exact_multiple_of_length
+        leftover = seq.seq[start .. -1]
+        print_seq.call(seq, leftover, start, fragment_number, total_fragment_count)
+      end
     end
   end
 end #end if running as a script
--- test/test_sequence_chunks.rb
+++ test/test_sequence_chunks.rb
@@ -17,28 +17,57 @@ path_to_script = File.join(File.dirname(__FILE__),'..',script_under_test)
 describe script_under_test do
   it 'when whole length is exactly a multiple of chunk size' do
     seqs = %w(>scaffold1 AANNTGTG)
-    
+
     Open3.popen3("#{path_to_script} -c 4 -s 4") do |stdin, stdout, stderr|
       stdin.puts seqs.join("\n")
       stdin.close
-      
+
       err = stderr.readlines
       err.should eq([]), err.join("")
       answer = %w(>scaffold1_1of2_1_4 AANN >scaffold1_2of2_5_8 TGTG).join("\n")+"\n"
       stdout.read.should eq(answer)
     end
   end
-  
+
   it 'when whole length is not exactly a multiple of chunk size' do
     seqs = %w(>scaffold1 AANNTGTG)
-    
+
     Open3.popen3("#{path_to_script} -c 4 -s 3") do |stdin, stdout, stderr|
       stdin.puts seqs.join("\n")
       stdin.close
-      
+
       err = stderr.readlines
       err.should eq([]), err.join("")
       answer = %w(>scaffold1_1of3_1_4 AANN >scaffold1_2of3_4_7 NTGT >scaffold1_3of3_5_8 TGTG).join("\n")+"\n"
+      stdout.read.should eq(answer)
+    end
+  end
+
+  it 'bugs out, not quite sure how to solve this one' do
+    seqs = %w(>scaffold1 AANNXTGTGXATGCAG)
+
+    Open3.popen3("#{path_to_script} -c 10 -s 5") do |stdin, stdout, stderr|
+      stdin.puts seqs.join("\n")
+      stdin.close
+
+      err = stderr.readlines
+      err.should be_nil
+      answer = %w(>scaffold1_1of3_1_10 AANNXTGTGX >scaffold1_2of3_6_15 TGTGXATGCA >scaffold1_3of3_7_16 GTGXATGCAG).join("\n")+"\n"
+      stdout.read.should eq(answer)
+    end
+  end
+
+  it 'should be ok printing the ends only' do
+    seqs = %w(>scaffold1 AANNXTGTGXATGCAG >scaffold2 TAANNXTGTGXATGCAGT)
+
+    Open3.popen3("#{path_to_script} --ends-only -c 3") do |stdin, stdout, stderr|
+      stdin.puts seqs.join("\n")
+      stdin.close
+
+      err = stderr.readlines
+      err.should eq([]), err
+      answer = %w(>scaffold1_start AAN >scaffold1_end CAG
+      >scaffold2_start TAA >scaffold2_end AGT).join("\n")+"\n"
       stdout.read.should eq(answer)
     end
   end
