f2fb683a928e4c927e43f8ff7950801b1688165a
Ben J. Woodcroft <donttrustben near gmail.com>
2013-12-04 09:59:40 +1000

    add option to remove chunks from break random seqs

--- break_random_sequences.rb
+++ break_random_sequences.rb
@@ -12,6 +12,8 @@ options = {
   :logger => 'stderr',
   :log_level => 'info',
   :min_piece_length => 1000,
+  :chunk_removal_min => 0,
+  :chunk_removal_max => 0,
 }
 o = OptionParser.new do |opts|
   opts.banner = "
@@ -19,11 +21,21 @@ o = OptionParser.new do |opts|
 
     Take a fasta file, and randomly split some number of them into two parts\n\n"
 
-  opts.on("-n", "--number-to-split NUM", "split these number of sequences up [required]") do |arg|
+  opts.on("-n", "--number-to-split NUM", Integer, "split these number of sequences up [required]") do |arg|
     options[:number_to_split] = arg.to_i
   end
-  opts.on("--min-length LENGTH", "don't split contigs up into pieces less than this long [default #{options[:min_piece_length]}]") do |arg|
+  opts.on("--min-length LENGTH", Integer, "don't split contigs up into pieces less than this long [default #{options[:min_piece_length]}]") do |arg|
     options[:min_piece_length] = arg.to_i
+  end
+  opts.on("--remove-chunks LENGTH1,LENGTH2", Array, "As well as breaking the sequence, remove some of it too. Choose a random amount of sequence between the two lengths given to remove. [default: don't remove]") do |args|
+    if args.length != 2
+      raise "Bad argument for --remove-chunks: #{args.inspect}"
+    end
+    options[:chunk_removal_min] = args[0].to_i
+    options[:chunk_removal_max] = args[1].to_i
+    unless options[:chunk_removal_min] <= options[:chunk_removal_max]
+      raise "max chunk removal size smaller than min, I'm confused!"
+    end
   end
 
   # logger options
@@ -42,7 +54,7 @@ Bio::Log::CLI.logger(options[:logger]); Bio::Log::CLI.trace(options[:log_level])
 fasta_file = ARGV[0]
 
 # Count the number of sequences in the fasta file that are greater than twice the minimum length
-sufficient_length = 2*options[:min_piece_length]+1
+sufficient_length = 2*options[:min_piece_length]+1+options[:chunk_removal_max]
 num_seqs_with_sufficient_length = 0
 log.debug "Reading from fasta file #{fasta_file}"
 Bio::FlatFile.foreach(fasta_file) do |seq|
@@ -52,6 +64,7 @@ Bio::FlatFile.foreach(fasta_file) do |seq|
     log.debug "Sequence accepted" if log.debug?
   end
 end
+log.info "Found #{num_seqs_with_sufficient_length} sequences of sufficient length"
 
 
 # Ensure that the number to split is less than the number of entries
@@ -72,12 +85,13 @@ Bio::FlatFile.foreach(fasta_file) do |seq|
 
     # If this is chosen to be split, split it
     if chosen_indices.include?(sufficient_index)
-      split_point = random_gen.rand(seq.seq.length-2*options[:min_piece_length]) + options[:min_piece_length]
+      chunk_removal_size = random_gen.rand(options[:chunk_removal_max]-options[:chunk_removal_min]) + options[:chunk_removal_min]
+      split_point = random_gen.rand(seq.seq.length-2*options[:min_piece_length]-chunk_removal_size) + options[:min_piece_length]
       seq1 = seq.seq[0...split_point]
-      seq2 = seq.seq[split_point...seq.seq.length]
-      puts ">#{seq.definition}_break1_#{split_point}"
+      seq2 = seq.seq[(split_point+chunk_removal_size)...seq.seq.length]
+      puts ">#{seq.definition}_break1_#{split_point}_#{chunk_removal_size}"
       puts seq1
-      puts ">#{seq.definition}_break2_#{split_point}"
+      puts ">#{seq.definition}_break2_#{split_point}_#{chunk_removal_size}"
       puts seq2
     else
       # Else just print it
