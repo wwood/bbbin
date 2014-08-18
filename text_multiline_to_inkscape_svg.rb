dc7a29aa9f11f6c0cc07a1f8081f4cf00f1f243a
Ben Woodcroft <donttrustben@gmail.com>
2014-08-19 08:31:06 +1000

    text_multiline_to_inkscape_svg.rb

---
+++ text_multiline_to_inkscape_svg.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+SCRIPT_NAME = File.basename(__FILE__); LOG_NAME = SCRIPT_NAME.gsub('.rb','')
+
+# Parse command line options into the options hash
+options = {
+}
+o = OptionParser.new do |opts|
+  opts.banner = "
+    Usage: #{SCRIPT_NAME}
+
+Takes lines of text on stdin, and prints out them in a form suitable for pasting into an Inkscape .svg file, where each line is in a separate text object\n\n"
+end; o.parse!
+
+#TODO what are you looking at me for? This is your script. Do something.
+y = 0
+ARGF.each do |line|
+  puts "<text y=\"#{y}\">#{line.chomp}</text>"
+  y += 10
+end
