3f11c1a22da263292b4b85e8305f81661b6a1c14
Ben Woodcroft <donttrustben@gmail.com>
2017-08-18 21:31:37 +1000

    randomFasta.pl: Add ability to print multiple sequences.

--- randomFasta.pl
+++ randomFasta.pl
@@ -3,19 +3,24 @@
 
 # Generate a sequence of random fasta files with arbitrary length
 
-if ($#ARGV != 0){
-  print "usage: $0 length\n".
-    "length is the number of base pairs you want to be in the sequence. Output is piped to STDOUT\n\n";
+if ($#ARGV != 0 and $#ARGV != 1){
+  print "usage: $0 length [num]\n".
+    "length is the number of base pairs you want to be in the sequence, num is the number of them (default 1). Output is piped to STDOUT\n\n";
 }
 
 $num = $ARGV[0];
 @bases = ('A','G','T','C');
 
-print ">random_sequence_length_$num\n";
-
-foreach (1..$num){
-  $i = int rand 4;
-  print $bases[$i];
+$num_total = 1;
+if ($#ARGV == 1){
+    $num_total = $ARGV[1]+0;
 }
 
-print "\n";
+foreach(1..$num_total){
+    print ">random_sequence_length_$num\n";
+    foreach (1..$num){
+        $i = int rand 4;
+        print $bases[$i];
+    }
+    print "\n";
+}
