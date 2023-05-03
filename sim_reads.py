7608f086ecc8da7d9ac95d4e9ad81da3304f41c0
Ben Woodcroft <benjwoodcroft@gmail.com>
2023-05-04 07:38:03 +1000

    sim_reads, base.yml, instrain_vcf -> lorikeet-benchmarking

--- sim_reads.py
+++ sim_reads.py
@@ -12,14 +12,9 @@ __maintainer__ = 'David Wood'
 __email__ = 'davidlawood@gmail.com'
 __status__ = 'Development'
 
-import os
 import sys
 import csv
-import shutil
-import tempfile
 import argparse
-import uuid
-import logging
 
 csv.field_size_limit(sys.maxsize)
 
@@ -63,14 +58,18 @@ class ReadSimulator(object):
         """Simulate reads."""
         fragment_len = 2*read_len + insert_size
         pair_count = 0
-        for seq_id, seq, _ in SequenceIO().each(input_io):
+        for _, seq, _ in SequenceIO().each(input_io):
             for i in range(0, len(seq) - fragment_len, read_step_size):
                 pair_count += 1
                 fout1.write('>%d\n' % pair_count)
                 fout1.write('%s\n' % seq[i:i+read_len])
-                fout2.write('>%d\n' % pair_count)
-                fout2.write('%s\n' % seq[i+read_len+insert_size:i+2*read_len+insert_size])
-        print("Write %i read pairs" % pair_count)
+                if fout2 is not None:
+                    fout2.write('>%d\n' % pair_count)
+                    fout2.write('%s\n' % seq[i+read_len+insert_size:i+2*read_len+insert_size])
+        if fout2 is None:
+            print("Write %i single-ended reads" % pair_count)
+        else:
+            print("Write %i read pairs" % pair_count)
 
 
 if __name__ == '__main__':
@@ -79,7 +78,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-i', help='file to simulate reads across', required=True)
     parser.add_argument('-1', '--output_read1', help='output read1 file', required=True)
-    parser.add_argument('-2', '--output_read2', help='output read2 file', required=True)
+    parser.add_argument('-2', '--output_read2', help='output read2 file')
 
     parser.add_argument('--read_len', type=int, help='length of simulated reads', default=150)
     parser.add_argument('--step_size', type=int, help='simulate a read each X bp', default=10)
@@ -88,5 +87,8 @@ if __name__ == '__main__':
 
     with open(args.i) as in_io:
         with open(args.output_read1,'w') as o1:
-            with open(args.output_read2,'w') as o2:
-                ReadSimulator.simulate_reads(in_io, args.read_len, args.step_size, args.insert_size, o1, o2)
+            if args.output_read2 is None:
+                ReadSimulator.simulate_reads(in_io, args.read_len, args.step_size, args.insert_size, o1, None)
+            else:
+                with open(args.output_read2,'w') as o2:
+                    ReadSimulator.simulate_reads(in_io, args.read_len, args.step_size, args.insert_size, o1, o2)
